c2079ecebef27002097939ed3480d83696552acf
Techcable <git@techcable.net>
2025-08-24 23:02:52 -0700

    increasing-array: Handle both lines of input
    Add first three official tests.
    
    This fixes the first failure

--- intro/004-increasing-array.rs
+++ intro/004-increasing-array.rs
@@ -1,9 +1,14 @@
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::io::read_to_string(std::io::stdin())?;
-    let numbers = input
+    let mut lines = input.lines();
+    let n = lines.next().unwrap().trim().parse::<usize>()?;
+    let numbers = lines
+        .next()
+        .unwrap()
         .split_whitespace()
         .map(|x| x.parse::<u64>())
         .collect::<Result<Vec<_>, _>>()?;
+    assert_eq!(numbers.len(), n);
     println!("{}", moves_needed(&numbers));
     Ok(())
 }
@@ -28,5 +33,23 @@ mod test {
     #[test]
     fn example() {
         assert_eq!(moves_needed(&[3, 2, 5, 1, 7]), 5);
+    }
+
+    #[test]
+    fn official_1() {
+        assert_eq!(moves_needed(&[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), 0);
+    }
+
+    #[test]
+    fn official_2() {
+        assert_eq!(
+            moves_needed(&[1000000000, 1, 1, 1, 1, 1, 1, 1, 1, 1,]),
+            8999999991
+        );
+    }
+
+    #[test]
+    fn official_3() {
+        assert_eq!(moves_needed(&[6, 10, 4, 10, 2, 8, 9, 2, 7, 7]), 31);
     }
 }
