5dca1bacabd049d878221e33914ee1c187aa23eb
Techcable <git@techcable.net>
2025-11-07 14:48:06 -0700

    intro/009: Solve by properly counting factors with multiples
    The algorithm runs in log(range.end) time.

--- intro/010-trailing-zeroes.rs
+++ intro/010-trailing-zeroes.rs
@@ -4,7 +4,7 @@
 //! So either a multiple of 10 appears in the expansion of `n!`,
 //! or a multiple of 5 along with a multiple of 2.
 
-use std::ops::Range;
+use std::ops::{RangeInclusive, RangeToInclusive};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::io::read_to_string(std::io::stdin())?;
@@ -13,85 +13,86 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+pub const MAX_INPUT: u32 = 10u32.pow(9);
+
 fn problem(n: u32) -> u64 {
-    let range = || 1..(n + 1);
-    let factors_of_10 = sum_range_step(range(), 10) / 10;
-    let factors_of_5 = sum_range_step(range(), 5) / 5;
-    let factors_of_2 = sum_range_step(range(), 2) / 2;
+    let factors_of_10 = count_factors_with_multiplicity(..=n, 10);
+    let factors_of_5 = count_factors_with_multiplicity(..=n, 5) - factors_of_10;
+    let factors_of_2 = count_factors_with_multiplicity(..=n, 2) - factors_of_10;
+    println!("{n} => fac 10 {factors_of_10}, fac 5 {factors_of_5}, fac 2 {factors_of_2}");
     factors_of_10 + factors_of_5.min(factors_of_2)
 }
 
-//
-// range math
-//
-
-/// Sum all the values in the specified range, stepping by a certain amount.
-#[inline]
-pub fn sum_range_step(orig: Range<u32>, step: u32) -> u64 {
-    // The following observation tells us how to reduce step>1 to step=1:
-    //   sum_range_step(0..=21, 5) = 0 + 5 + 10 + 15 + 20 = 5(0 + 1 + 2 + 3 + 4)
-    // This shows us how to deal with non-zero start:
-    //   sum_range_step(6..20, 5) = 6 + 11 + 16 = (6 * 3) + 5(0 + 1 + 2)
-    //
-    // Now all together:
-    assert_ne!(step, 0);
-    let orig_len = orig.end - orig.start;
-    let len = ((orig_len - 1) / step) // elements after start
-        + 1; // start element
-    debug_assert_eq!(
-        orig.clone().step_by(step as usize).len() as u64,
-        len as u64,
-        "{orig:?}.step_by({step}), values = {:?}, orig_len = {orig_len}",
-        orig.clone().step_by(step as usize).collect::<Vec<u32>>()
-    );
-    (len as u64 * orig.start as u64) + (sum_range(0..len) * (step as u64))
-}
-
-/// Sum all the values in the specified range.
-///
-/// Equivalent to `range.sum()` but takes O(1) time.
-#[inline]
-pub fn sum_range(range: Range<u32>) -> u64 {
-    let len = (range.end - range.start) as u64;
-    // special-case is needed to avoid integer underflow
-    if len == 0 {
-        0
-    } else {
-        (len * range.start as u64) + ((len * (len - 1)) / 2)
+/// This algorithm runs in `log(range.end)` time.
+fn count_factors_with_multiplicity(range: RangeToInclusive<u32>, factor: u32) -> u64 {
+    if range.end < factor {
+        return 0;
     }
+    let max_power = range.end.ilog(factor);
+    debug_assert!(
+        (factor as u64).pow(max_power + 1) >= range.end as u64,
+        "{factor}^{max_power} is not maximum power of {range:?}"
+    );
+    let mut count: u64 = 0;
+    for power in 1..=max_power {
+        let value = factor.pow(power);
+        // we are only counting the additional factors added by consideration of `value`
+        count += range_step_len(value..=range.end, value) as u64;
+    }
+    count
 }
 
-#[inline] // want modulo reduced to shift+add when constant
-#[allow(clippy::cast_possible_truncation)] // not possible since modulo is u32
-pub fn mul_mod(a: u32, b: u32, modulo: u32) -> u32 {
-    (u64::from(a).wrapping_mul(b as u64) % (modulo as u64)) as u32
+/// Calculates `range.step_by(step).count()` in O(1) time.
+#[inline]
+fn range_step_len(range: RangeInclusive<u32>, step: u32) -> u32 {
+    assert_ne!(step, 0);
+    let len = (range.end() - range.start()) + 1;
+    let elements_after_start = (len - 1) / step;
+    elements_after_start + 1
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::ops::Range;
 
     #[test]
-    fn sum_range_step_verify() {
+    fn range_step_len() {
         #[track_caller]
-        fn verify(x: Range<u32>, step: u32) {
+        fn verify(x: RangeInclusive<u32>, step: u32) {
             assert_eq!(
-                super::sum_range_step(x.clone(), step),
-                x.clone().step_by(step as usize).map(u64::from).sum::<u64>(),
-                "Unexpected sum for {x:?}.step_by({step})"
+                super::range_step_len(x.clone(), step) as usize,
+                x.clone().step_by(step as usize).count(),
+                "Unexpected length for {x:?}.step_by({step})"
             );
         }
-        verify(0..20, 5);
-        verify(7..18, 2);
-        verify(10..100, 3);
-        verify(0..97, 7);
-        verify(0..95, 7);
-        verify(0..101, 7);
+        verify(0..=2, 2);
+        verify(0..=20, 5);
+        verify(0..=19, 5);
+        verify(7..=18, 2);
+        verify(10..=100, 3);
+        verify(0..=97, 7);
+        verify(0..=95, 7);
+        verify(0..=101, 7);
     }
 
     #[test]
     fn example() {
         assert_eq!(problem(20), 4);
+    }
+
+    #[test]
+    fn timeout_max_input() {
+        let _ = problem(MAX_INPUT);
+    }
+
+    #[test]
+    fn official_tests() {
+        assert_eq!(problem(395), 97); // test1
+        assert_eq!(problem(850915850), 212728957); // test2
+        assert_eq!(problem(871), 215); // test3
+        assert_eq!(problem(239), 57); // test4
+        assert_eq!(problem(850915850), 212728957); // test8
+        assert_eq!(problem(19273478), 4818363); // test9
+        assert_eq!(problem(669763357), 167440831); // test10
     }
 }
