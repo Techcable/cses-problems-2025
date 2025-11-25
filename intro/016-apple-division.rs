1eee8ae294ef5e6159dcfb605f81dbb72dd5fa63
Techcable <git@techcable.net>
2025-11-25 13:36:53 -0700

    intro/016: Consider pairs during minimization
    Problem: Don't just want to consider pairs where values are close together.
    This will minimize the diff of the pair, but we really want diffs that are close to `target_delta`.
    Need to take this into consideration.
    
    Need to rework the `new_delta` function to produce a `relative_delta` s.t. `new_delta = old_delta + relative_delta`.

--- intro/016-apple-division.rs
+++ intro/016-apple-division.rs
@@ -14,7 +14,7 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(|entry| entry.parse::<u32>())
         .collect::<Result<Vec<_>, _>>()?;
     assert_eq!(num_inputs, apple_weights.len());
-    println!("{}", problem(&apple_weights).delta());
+    println!("{}", problem(&apple_weights).signed_delta());
     Ok(())
 }
 
@@ -24,10 +24,10 @@ pub const MAX_WEIGHT: u32 = 10u32.pow(9);
 pub fn problem(weights: &[u32]) -> Solution {
     let mut solution = Solution::begin(weights);
     loop {
-        let prev_delta = solution.delta();
+        let prev_delta = solution.abs_delta();
         match minimize_difference(solution) {
             Ok(reduced) => {
-                assert!(reduced.delta() < prev_delta);
+                assert!(reduced.abs_delta() < prev_delta);
                 solution = reduced;
             }
             Err(MinimizationFailedError(minimal)) => return minimal,
@@ -37,85 +37,118 @@ pub fn problem(weights: &[u32]) -> Solution {
 
 /// Minimize the difference between the two values, returning
 fn minimize_difference(mut solution: Solution) -> Result<Solution, MinimizationFailedError> {
-    if solution.delta() == 0 {
+    if solution.signed_delta() == 0 {
         // already minimal
         return Err(MinimizationFailedError(solution));
     }
     assert!(!solution.left.is_empty() || !solution.right.is_empty());
-    let delta = solution.delta();
-    /// Minimize two optional values, returning `None` only if both inputs are `None`..
-    fn minimize_opt_by<T, K: Ord>(
-        x: Option<T>,
-        y: Option<T>,
-        mut func: impl FnMut(&T) -> K,
-    ) -> Option<T> {
-        match (x, y) {
-            (Some(a), Some(b)) => Some(if func(&a) < func(&b) { a } else { b }),
-            (Some(y), None) | (None, Some(y)) => Some(y),
-            (None, None) => None,
-        }
-    }
-    fn closest_of(set: &NumberSet, x: u64) -> Option<u32> {
-        let Ok(x) = u32::try_from(x) else {
-            return set.largest();
-        };
-        minimize_opt_by(
-            set.first_below(Bound::Excluded(x)),
-            set.first_above(Bound::Excluded(x)),
-            |y| y.abs_diff(x),
-        )
-    }
-    // Find a number in left, right closest to delta/2, then see which one that improves our solution
-    // rounding is not a problem here, because in the worst case true value is x.5 => x,
-    // and we end up picking x over (x + 1)
-    let half_delta = delta / 2;
-    fn delta_after_move(src: &NumberSet, dest: &NumberSet, value: u32) -> u64 {
-        debug_assert!(src.contains(value), "invalid move of {value}");
-        let value = value as u64;
-        (src.sum() - value).abs_diff(dest.sum() + value)
-    }
-    fn delta_move_to_right(set: &Solution, value: u32) -> u64 {
-        delta_after_move(&set.left, &set.right, value)
-    }
-    fn delta_move_to_left(set: &Solution, value: u32) -> u64 {
-        delta_after_move(&set.right, &set.left, value)
-    }
-    // find the closest value we want to move, normalizing the solution so that we move from left to right
-    let (closest, new_delta) = {
-        let closest_left = closest_of(&solution.left, half_delta);
-        let closest_right = closest_of(&solution.right, half_delta);
-        match (closest_left, closest_right) {
-            (Some(closest_left), Some(closest_right)) => {
-                let delta_move_to_right = delta_move_to_right(&solution, closest_left);
-                let delta_move_to_left = delta_move_to_left(&solution, closest_right);
-                if delta_move_to_left < delta_move_to_right {
-                    // swap so that the left has the closest value and we move to the right
-                    solution.swap();
-                    (closest_right, delta_move_to_left)
-                } else {
-                    (closest_left, delta_move_to_right)
-                }
-            }
-            (Some(closest_left), None) => {
-                (closest_left, delta_move_to_right(&solution, closest_left))
-            }
-            (None, Some(closest_right)) => {
-                solution.swap();
-                (closest_right, delta_move_to_right(&solution, closest_right))
-            }
-            (None, None) => unreachable!("both sets are empty for {solution:?}"),
-        }
+    let orig_delta = solution.signed_delta();
+    let new_delta = |pair: Pair| {
+        (solution.left.sum() as i64 - pair.0 as i64 + pair.1 as i64)
+            - (solution.right.sum() as i64 - pair.1 as i64 + pair.0 as i64)
     };
-    if new_delta < delta {
-        solution.move_to_right(closest);
+    let smallest_pair = meaningful_pairs(&solution)
+        .min_by_key(|&pair| {
+            // println!("considering {pair:?} with {}", new_delta(pair));
+            new_delta(pair).abs()
+        })
+        .unwrap();
+    let new_delta = new_delta(smallest_pair);
+    // println!("found pair {smallest_pair:?} for {orig_delta} -> {new_delta}, {solution:?}");
+    smallest_pair.apply_to(&mut solution);
+    assert_eq!(
+        solution.signed_delta(),
+        new_delta,
+        "bad new solution {solution:?} after {smallest_pair:?}"
+    );
+    if new_delta.abs() < orig_delta.abs() {
         Ok(solution)
     } else {
+        smallest_pair.reversed().apply_to(&mut solution); // revert the change
         Err(MinimizationFailedError(solution))
     }
 }
 
 /// Indicates the solution cannot be minimized further.
 struct MinimizationFailedError(Solution);
+
+/// Indicates a pair of values in a [`Solution`] that should be swapped,
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Pair(u32, u32);
+impl Pair {
+    pub fn reversed(self) -> Pair {
+        Pair(self.1, self.0)
+    }
+    pub fn apply_to(self, sol: &mut Solution) {
+        assert_ne!(self, Pair(0, 0), "zero pair is meaningless");
+        assert!(
+            self.0 == 0 || sol.left.contains(self.0),
+            "missing value {a} for {self:?}, {sol:?}",
+            a = self.0
+        );
+        assert!(
+            self.1 == 0 || sol.right.contains(self.1),
+            "missing value {b} for {self:?}, {sol:?}",
+            b = self.1
+        );
+        if self.0 != 0 {
+            sol.move_to_right(self.0);
+        }
+        if self.1 != 0 {
+            sol.move_to_left(self.1);
+        }
+    }
+}
+fn meaningful_pairs(sol: &Solution) -> impl Iterator<Item = Pair> + '_ {
+    assert!(
+        !sol.left.is_empty() || !sol.right.is_empty(),
+        "both sets are empty"
+    );
+    assert_ne!(sol.left.smallest(), Some(0), "zero entry is forbidden");
+    assert_ne!(sol.right.smallest(), Some(0), "zero entry is forbidden");
+    assert_ne!(sol.left.smallest(), sol.right.smallest());
+    let zero_pairs = [
+        sol.left.smallest().map(|val| Pair(val, 0)),
+        sol.right.smallest().map(|val| Pair(0, val)),
+    ]
+    .into_iter()
+    .flatten();
+    let nonzero_left_pairs = sol.left.iter().filter_map(move |left_value| {
+        let closest_right = closest_of(&sol.right, left_value)?;
+        assert_ne!(
+            closest_right, left_value,
+            "both sets contain value: {sol:?}"
+        );
+        Some(Pair(left_value, closest_right))
+    });
+    let nonzero_right_pairs = sol.right.iter().filter_map(move |right_value| {
+        let closest_left = closest_of(&sol.left, right_value)?;
+        Some(Pair(closest_left, right_value))
+    });
+    zero_pairs
+        .chain(nonzero_left_pairs)
+        .chain(nonzero_right_pairs)
+}
+
+/// Minimize two optional values, returning `None` only if both inputs are `None`..
+fn minimize_opt_by<T, K: Ord>(
+    x: Option<T>,
+    y: Option<T>,
+    mut func: impl FnMut(&T) -> K,
+) -> Option<T> {
+    match (x, y) {
+        (Some(a), Some(b)) => Some(if func(&a) < func(&b) { a } else { b }),
+        (Some(y), None) | (None, Some(y)) => Some(y),
+        (None, None) => None,
+    }
+}
+fn closest_of(set: &NumberSet, x: u32) -> Option<u32> {
+    minimize_opt_by(
+        set.first_below(Bound::Excluded(x)),
+        set.first_above(Bound::Excluded(x)),
+        |y| y.abs_diff(x),
+    )
+}
 
 #[derive(Debug, Clone)]
 pub struct Solution {
@@ -143,8 +176,12 @@ impl Solution {
         self.right.remove(value);
         self.left.insert(value);
     }
+    /// The delta for this pair, which is how much bigger the `left` sum is than the `right` sum.
+    pub fn signed_delta(&self) -> i64 {
+        (self.left.sum() as i64) - (self.right.sum() as i64)
+    }
     #[inline]
-    pub fn delta(&self) -> u64 {
+    pub fn abs_delta(&self) -> u64 {
         self.left.sum().abs_diff(self.right.sum())
     }
 }
@@ -153,7 +190,7 @@ pub fn naive_problem(weights: &[u32]) -> Solution {
     let mut begin = Solution::begin(weights);
     let mut min_result = begin.clone();
     let _ = naive_search(&mut begin, &mut |solution| {
-        if solution.delta() < min_result.delta() {
+        if solution.abs_delta() < min_result.abs_delta() {
             min_result = solution.clone();
         }
         ControlFlow::Continue(())
@@ -391,7 +428,7 @@ mod test {
     fn verify_with(inputs: &[u32], expected_result: u64, problem: fn(&[u32]) -> Solution) {
         let actual_result = problem(inputs.as_ref());
         assert_eq!(
-            actual_result.delta(),
+            actual_result.abs_delta(),
             expected_result,
             "Unexpected result {actual_result:?} for {inputs:?}"
         )
@@ -417,5 +454,27 @@ mod test {
     #[test]
     fn test1() {
         verify([603, 324, 573, 493, 659, 521, 654, 70, 718, 257], 2);
+    }
+
+    #[test]
+    fn test2() {
+        verify([952, 775, 292, 702, 859, 719, 65, 943, 376, 490], 1);
+    }
+
+    #[test]
+    fn test3() {
+        verify([141, 156, 14, 487, 250, 230, 741, 602, 32, 717], 2);
+    }
+
+    #[test]
+    fn test10() {
+        verify(
+            [
+                92021619, 792314463, 937735495, 807418830, 589829609, 270804901, 94470968,
+                853138862, 817966179, 656206734, 121829906, 137518261, 766263169, 320158086,
+                167044551, 860185204, 347066817, 401533984, 755773385, 623993044,
+            ],
+            4453,
+        );
     }
 }
