c81c10d14e5dc43905651608ff8a5fe1b1ad717b
Techcable <git@techcable.net>
2026-03-09 08:21:16 -0700

    intro/016: Print extra info on NICKNINJA_DEBUG=1

--- intro/016-apple-division.rs
+++ intro/016-apple-division.rs
@@ -12,7 +12,12 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
         .map(str::parse::<u32>)
         .collect::<Result<Vec<_>, _>>()?;
     assert_eq!(num_inputs, apple_weights.len());
-    println!("{}", problem(&apple_weights).signed_delta());
+    let sol = naive_problem(&apple_weights);
+    println!("{}", sol.signed_delta());
+    if std::env::var_os("NICKNINJA_DEBUG").is_some_and(|x| x == "1") {
+        eprintln!("{:?}", sol.left);
+        eprintln!("{:?}", sol.right);
+    }
     Ok(())
 }
 
